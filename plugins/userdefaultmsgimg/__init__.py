747fe036ca18510bc5ae8ee5a60f42eefc34ce15
jianxiong.cao <jianxiong.cao@shopee.com>
2024-07-26 10:38:52 +0800

    feat: UserDefaultMsgImg v1.1

--- plugins/userdefaultmsgimg/__init__.py
+++ plugins/userdefaultmsgimg/__init__.py
@@ -1,22 +1,36 @@
-import inspect
 from app.chain import ChainBase
 from functools import wraps
 from app.schemas import Notification
 from app.plugins import _PluginBase
 from typing import Any, List, Dict, Tuple
 from app.log import logger
+from app.chain.tmdb import TmdbChain
+from app.utils.web import WebUtils
+from app.core.config import settings
 
 
-def add_default_attr(method, attr_name, default_value: str):
+def add_default_attr(method, img_link: str | None):
     @wraps(method)
     def wrapper(self, message: Notification):
-        if not hasattr(message, attr_name) or getattr(message, attr_name) is None:
-            setattr(message, attr_name, default_value)
+        if not hasattr(message, "image") or getattr(message, "image") is None:
+            img_url = img_link
+            if not isinstance(img_url, str) or not len(img_url) > 0:
+                if settings.WALLPAPER == "tmdb":
+                    url = TmdbChain().get_random_wallpager()
+                else:
+                    url = WebUtils.get_bing_wallpaper()
+                logger.info(f"tmdb_url:{url}, {url}")
+                img_url = url if isinstance(url, str) and url else \
+                    "https://raw.githubusercontent.com/jianxcao/MoviePilot-extension/main/img/mp.jpg"
+            setattr(message, "image", img_url)
+            logger.info(f"image_url: {img_url}")
         return method(self, message)
 
     return wrapper
 
+
 old_post_message = ChainBase.post_message
+
 
 class UserDefaultMsgImg(_PluginBase):
     # 插件名称
@@ -26,7 +40,7 @@ class UserDefaultMsgImg(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jianxcao/MoviePilot-extension/main/img/img.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "jianxcao"
     # 加载顺序
@@ -36,6 +50,7 @@ class UserDefaultMsgImg(_PluginBase):
     # 私有属性
     _img_link = ''
     _enabled = False
+    __post_message_width_img = None
 
     def init_plugin(self, config: dict = None):
         # 停止现有任务
@@ -44,9 +59,7 @@ class UserDefaultMsgImg(_PluginBase):
             self._img_link = config.get('img_link')
             self._enabled = config.get('enabled')
             self.__update_config()
-            if not isinstance(self._img_link, str) or not len(self._img_link) > 0:
-                self._img_link = "https://raw.githubusercontent.com/jianxcao/MoviePilot-extension/main/img/mp.jpg"
-            self.__post_message_width_img = add_default_attr(old_post_message, "image", self._img_link)
+            self.__post_message_width_img = add_default_attr(old_post_message, self._img_link)
             self.__post_message_re_define()
 
     def __update_config(self):
